4b47f099f343e7f2e2b2dab8572b30bafadac9c5
pchiarato <phillip.chiarato@gmail.com>
2018-09-23 17:10:56 -0400

    Added Jwt token

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
-namespace DatingApp.API.Dtos
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos // data transfer object.
 {
   public class UserForRegisterDto
   {
+    [Required]
     public string Username { get; set; }
+
+    [Required]
+    [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters.")]
     public string Password { get; set; }
   }
 }
